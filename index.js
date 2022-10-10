344af462505796231dd3800633c22ad86721a62c
NanHaiJuRuo <ygyh5019@outlook.com>
2022-10-10 17:35:16 +0800

    1.0.1

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ onInit(){api.addCategory({categoryId: catId, messageId: catId, color: '#FFAB19'}
 api.addBlock({
     function: ()=>{api.getVmInstance().greenFlag()}, 
     type:type.BlockType.COMMAND, 
+    option: {terminal: true},
     opcode:catId+'.greenFlag', messageId:catId+'.greenFlag', 
     categoryId:catId
 })
