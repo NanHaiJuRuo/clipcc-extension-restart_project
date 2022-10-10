128a75af41d946336e301c05dcb98dbf2ce404d9
NanHaiJuRuo <ygyh5019@outlook.com>
2022-10-10 12:53:33 +0800

    1.0.0

---
+++ README.md
@@ -0,0 +1,8 @@
+<img src="assets/icon.webp">
+
+#### restart_project Extension
+greenFlag()
+
+***
+
+Download it? [dist/](dist/)
---
+++ index.js
@@ -0,0 +1,5 @@
+const catId='nhjr.restart_project';
+const {api, type, Extension}=require('clipcc-extension');class E extends Extension{
+onInit(){api.addCategory({categoryId: catId, messageId: catId, color: '#FFAB19'});
+api.addBlock({function: ()=>{api.getVmInstance().greenFlag()}, type:type.BlockType.COMMAND, opcode:catId+'.greenFlag', messageId:catId+'.greenFlag', categoryId:catId})
+}onUninit(){api.removeCategory(catId)}}module.exports=E
