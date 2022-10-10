ede3843e4d261dac012c7a34e970c28d3d4ab5a2
NanHaiJuRuo <ygyh5019@outlook.com>
2022-10-10 13:06:28 +0800

    (

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const catId='nhjr.restart_project';
-const {api, type, Extension}=require('clipcc-extension');class E extends Extension{
+const {api, type, Extension}=require('clipcc-extension');
+class E extends Extension{
 onInit(){api.addCategory({categoryId: catId, messageId: catId, color: '#FFAB19'});
-api.addBlock({function: ()=>{api.getVmInstance().greenFlag()}, type:type.BlockType.COMMAND, opcode:catId+'.greenFlag', messageId:catId+'.greenFlag', categoryId:catId})
+api.addBlock({
+    function: ()=>{api.getVmInstance().greenFlag()}, 
+    type:type.BlockType.COMMAND, 
+    opcode:catId+'.greenFlag', messageId:catId+'.greenFlag', 
+    categoryId:catId
+})
 }onUninit(){api.removeCategory(catId)}}module.exports=E
